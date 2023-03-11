36dc6fda2fa39f1131a90cc63b3bda8bd26e42ac
JeongYunLee <cathx618@gmail.com>
2023-03-11 19:15:37 +0900

    create new version

---
+++ .nuxt/components/index.js
@@ -0,0 +1,44 @@
+export { default as AuthorCard } from '../../components/AuthorCard.vue'
+export { default as AuthorIntro } from '../../components/AuthorIntro.vue'
+export { default as BuyMeACoffee } from '../../components/BuyMeACoffee.vue'
+export { default as Expertise } from '../../components/Expertise.vue'
+export { default as ProjectCard } from '../../components/ProjectCard.vue'
+export { default as TheFooter } from '../../components/TheFooter.vue'
+export { default as TheHeader } from '../../components/TheHeader.vue'
+export { default as TimeLine } from '../../components/TimeLine.vue'
+export { default as BlogItem } from '../../components/blogItem.vue'
+export { default as ButtonMC } from '../../components/buttonMC.vue'
+export { default as ColorMC } from '../../components/colorMC.vue'
+export { default as Darkmode } from '../../components/darkmode.vue'
+export { default as Lightmode } from '../../components/lightmode.vue'
+
+// nuxt/nuxt.js#8607
+function wrapFunctional(options) {
+  if (!options || !options.functional) {
+    return options
+  }
+
+  const propKeys = Array.isArray(options.props) ? options.props : Object.keys(options.props || {})
+
+  return {
+    render(h) {
+      const attrs = {}
+      const props = {}
+
+      for (const key in this.$attrs) {
+        if (propKeys.includes(key)) {
+          props[key] = this.$attrs[key]
+        } else {
+          attrs[key] = this.$attrs[key]
+        }
+      }
+
+      return h(options, {
+        on: this.$listeners,
+        attrs,
+        props,
+        scopedSlots: this.$scopedSlots,
+      }, this.$slots.default)
+    }
+  }
+}
