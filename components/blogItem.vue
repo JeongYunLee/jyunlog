e502a1db296f6d45df748b30ae995ffc4c3296de
JeongYunLee <cathx618@gmail.com>
2023-06-02 10:36:51 +0900

    update 0602

--- components/blogItem.vue
+++ components/blogItem.vue
@@ -1,14 +1,10 @@
 <template>
   <div>
-    <article
-      class="relative flex flex-col max-w-3xl lg:ml-auto xl:max-w-none xl:w-[50rem]"
-    >
-      <h3
-        class="mb-4 text-4xl text-slate-700 tracking-tight font-bold dark:text-slate-200"
-      >
+    <article class="relative flex flex-col max-w-3xl lg:ml-auto xl:max-w-none xl:w-[50rem]">
+
+      <h3 class="mb-2 text-2xl text-slate-700 tracking-tight font-bold dark:text-slate-200">
         <NuxtLink :to="{ name: 'blog-slug', params: { slug: postSlug } }">
-          {{ postTitle }}</NuxtLink
-        >
+          {{ postTitle }}</NuxtLink>
       </h3>
 
       <div class="mb-6 prose prose-slate dark:prose-dark">
@@ -16,20 +12,20 @@
           {{ postDescription }}
         </p>
       </div>
+
+
       <div class="mt-auto flex flex-row-reverse items-center justify-end">
         <dl>
           <dt class="sr-only">Date</dt>
           <dd
-            class="text-sm leading-6 dark:text-slate-400 lg:absolute lg:top-0 lg:right-full lg:mr-8 lg:whitespace-nowrap font-bold"
-          >
-            <time
-              class="bg-green-500 text-white py-1 px-2 rounded-md"
-              datetime="2022-02-24T12:00:00.000Z"
-              >{{ formatDate(postDate) }}</time
-            >
+            class="text-sm leading-6 dark:text-slate-400 lg:absolute lg:top-0 lg:right-full lg:mr-8 lg:whitespace-nowrap font-bold">
+            <time class="bg-green-500 text-white py-1 px-2 rounded-md" datetime="2022-02-24T12:00:00.000Z">{{
+              formatDate(postDate) }}</time>
           </dd>
         </dl>
       </div>
+
+
       <br />
     </article>
   </div>
@@ -44,7 +40,7 @@ export default {
       postDescription: this.description,
       postSlug: this.slug,
       postDate: this.date,
-      posttag: this.tags,
+      postTag: this.tags,
     };
   },
   methods: {
@@ -52,6 +48,9 @@ export default {
       const options = { year: "numeric", month: "long", day: "numeric" };
       return new Date(date).toLocaleDateString("en", options);
     },
+  },
+  mounted() {
+    console.log("postTag:", this.postTag);
   },
 };
 </script>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,24 +1,16 @@
 <template>
   <div>
     <header class="pt-16 pb-9 sm:pb-16 sm:text-center">
-      <h1
-        class="mb-4 text-8xl sm:text-4xl tracking-tight text-slate-800 font-extrabold dark:text-slate-200"
-      >
+      <h1 class="mb-4 text-8xl sm:text-4xl tracking-tight text-slate-800 font-extrabold dark:text-slate-200">
         Blog
       </h1>
       <p class="text-lg text-slate-700 dark:text-slate-400">
         I write about web development, tech, and other things.
       </p>
     </header>
-    <div class="space-y-16 mx-auto max-w-7xl">
-      <blog-item
-        v-for="article in articles"
-        :key="article.title"
-        :title="article.title"
-        :description="article.description"
-        :date="article.date"
-        :slug="article.slug"
-      ></blog-item>
+    <div class="space-y-16 mx-auto max-w-6xl">
+      <blog-item v-for="article in articles" :key="article.title" :title="article.title"
+        :description="article.description" :date="article.date" :slug="article.slug"></blog-item>
     </div>
   </div>
 </template>
