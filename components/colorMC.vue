64de23ebca2688602fc54a4bf368d77377625f7c
JeongYunLee <cathx618@gmail.com>
2023-10-09 18:21:41 +0900

    edit colorMC image path

--- components/colorMC.vue
+++ components/colorMC.vue
@@ -1,5 +1,5 @@
 <template>
-    <img src="images/color.png" alt="color" style="height: 40px !important; width: 120px !important"
+    <img src="/images/color.png" alt="color" style="height: 40px !important; width: 120px !important"
         class="mt-2" />
 </template>
   
