052944fbf743b74acbea3c681969fab897f316aa
JeongYunLee <cathx618@gmail.com>
2024-01-18 17:27:51 +0900

    write 2024-01-16

--- .output/public/_nuxt/formatDate.b0fafd06.js
+++
@@ -1,1 +0,0 @@
-import{u as a}from"./config.d4eb953b.js";function i(t){const o=a().metadata,n={year:"numeric",month:"long",day:"numeric"};return new Date(t).toLocaleDateString(o.locale,n)}export{i as _};
