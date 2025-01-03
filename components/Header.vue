a4a8c6c967ec1bba5e3992d5125bc28a0816856f
JeongYunLee <cathx618@gmail.com>
2025-01-03 16:18:06 +0900

    edit main_bg path again

--- components/Header.vue
+++ components/Header.vue
@@ -13,8 +13,7 @@
     </div> -->
     <TransitionGroup name="list" tag="div" class="header-bg">
       <!-- <div v-for="rectang in columns" :key="rectang" :style="rectang"></div> -->
-      <img :class="showBgImg ? 'opacity-100' : 'opacity-0'" src="~static/main_bg.jpg" width="1920" height="1280"
-        alt="background image" />
+      <img :class="showBgImg ? 'opacity-100' : 'opacity-0'" src="/main_bg.jpg" width="1920" height="1280" alt="background image" />
     </TransitionGroup>
     <div class="content text-center" :class="showContent ? 'opacity-85' : 'opacity-0'">
       <h1 class="leading-3">
